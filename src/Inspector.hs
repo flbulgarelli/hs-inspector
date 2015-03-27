4e502e5483b30f5cd801875fa67dc92bf35c67a0
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-27 00:00:15 -0300

    Partially implementing composition detection

--- src/Inspector.hs
+++ src/Inspector.hs
@@ -11,7 +11,10 @@ type Code = String
 type Inspection = Binding -> Code  -> Bool
 
 hasComposition :: Inspection
-hasComposition _ _ = False
+hasComposition = testAnyWithBindingRhs f
+  where f rhs = exploreExprs g $ topExprs rhs
+        g (O (HsQVarOp (UnQual (HsSymbol ".")))) = True
+        g _ = False
 
 hasRecursion :: Inspection
 hasRecursion _ _ = False
@@ -57,5 +60,21 @@ withCode :: ([HsDecl] -> a) -> Code -> Maybe a
 withCode f code | ParseOk (HsModule _ _ _ _ decls) <- parseModule code = Just (f decls)
                 | otherwise = Nothing
 
-
 orFalse = fromMaybe False
+
+data EO = E HsExp | O HsQOp
+
+exploreExprs f exprs = exploreEO f $ map (E) exprs
+
+exploreEO f es = any f es || any (exploreEO' f) es
+
+exploreEO' :: (EO -> Bool) -> EO -> Bool
+exploreEO' f (E (HsInfixApp a b c)) = exploreEO f [E a, O b, E c]
+exploreEO' f (E (HsApp a b))  = exploreEO f [E a, E b]
+exploreEO' f (E (HsNegApp a))  = exploreEO f [E a]
+exploreEO' f (E (HsLambda _ _ a)) = exploreEO f [E a]
+exploreEO' f (E (HsList a)) = exploreEO f $ map (E) a
+exploreEO' _ _ = False
+
+topExprs (HsUnGuardedRhs e) = [e]
+topExprs (HsGuardedRhss rhss) = concatMap (\(HsGuardedRhs _ es1 es2) -> [es1, es2]) rhss
