3075fc8b4e8f1012a6096135c0d41a055b65cb60
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-05 17:07:17 -0300

    Adding some test for redundant lambda detection

--- spec/SmellSpec.hs
+++ spec/SmellSpec.hs
@@ -5,6 +5,17 @@ import           Language.Haskell.Inspector.Smell
 
 spec :: Spec
 spec = do
+  describe "hasRedundantLambda" $ do
+    it "is True whn η-conversion applies" $ do
+      hasRedundantLambda "x" "x = \\m -> f m" `shouldBe` True
+
+    it "is True whn η-conversion applies, whith composition " $ do
+      hasRedundantLambda "x" "x = \\m -> (f.g) m" `shouldBe` True
+
+    it "is False when it is an unavoidable lambda" $ do
+      hasRedundantLambda "x" "x = \\m -> m f" `shouldBe` False
+
+
   describe "hasRedundantIf" $ do
     it "is True when if present and both branches are boolean literals" $ do
       hasRedundantIf "x" "x = if m then True else False" `shouldBe` True
